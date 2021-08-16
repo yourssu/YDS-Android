1c34a35b15a867f83cc3ff7a70bbdc0ab960f50e
junghyun park <parkmint.urssu@gmail.com>
2021-08-16 15:03:06 +0900

    [YDS] 토스트 추가 & deprecate 메소드 버전 분기처리

--- DesignSystem/src/main/java/com/yourssu/design/undercarriage/base/BaseFullDialog.kt
+++ DesignSystem/src/main/java/com/yourssu/design/undercarriage/base/BaseFullDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
 import android.view.*
 import com.yourssu.design.R
@@ -19,10 +20,19 @@ abstract class BaseFullDialog: Dialog {
         init()
     }
 
+    @Suppress("DEPRECATION")
     private fun init() {
         window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
         window?.requestFeature(Window.FEATURE_NO_TITLE)
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window?.setDecorFitsSystemWindows(false)
+            val insetController = window?.insetsController
+            insetController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            insetController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_BARS_BY_SWIPE
+        } else {
+            window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
     }
 }
--- app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/MainActivity.kt
+++ app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ObservableBoolean
 import com.yourssu.design.system.atom.BottomSheet.Companion.bottomSheet
 import com.yourssu.design.system.atom.Text.Companion.text
+import com.yourssu.design.system.component.Toast
+import com.yourssu.design.system.component.Toast.Companion.shortToast
+import com.yourssu.design.system.component.Toast.Companion.toast
 import com.yourssu.design.system.foundation.Typo
 import com.yourssu.yds_ui_tester.databinding.ActivityMainBinding
 
@@ -35,6 +38,12 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }.show()
+            binding.root
+        }
+        binding.aaged.setOnClickListener {
+            toast("안녕", Toast.Short)
+            shortToast("안녕")
+            toast("안녕")
         }
     }
 }
