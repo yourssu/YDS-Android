dde9e18573de0a2719567526835b8429f78ba2f2
ByeongsuPark <byeongsu@soongsil.ac.kr>
2021-08-17 22:06:33 +0900

    [YDS-Toggle] Toggle 테스트 액티비티 추가

---
+++ app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/ToggleActivity.kt
@@ -0,0 +1,28 @@
+package com.yourssu.yds_ui_tester
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableBoolean
+import com.yourssu.yds_ui_tester.databinding.ActivityToggleBinding
+
+class ToggleActivity : AppCompatActivity() {
+    lateinit var binding: ActivityToggleBinding
+
+    var isDisabled = ObservableBoolean(false)
+    var isSelected = ObservableBoolean(false)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_toggle)
+        binding.activity = this
+
+        binding.buttonChangeDisabled.setOnClickListener {
+            isDisabled.set(!isDisabled.get())
+        }
+
+        binding.buttonChangeSelected.setOnClickListener {
+            isSelected.set(!isSelected.get())
+        }
+    }
+}
