91b3aed3a4e1acad028474c54eab2f12df8081b0
junghyun park <parkmint.urssu@gmail.com>
2021-08-31 16:30:55 +0900

    [YDS-Picker] Picker 구현 바텀시트 곡률 조정

--- app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/MainActivity.kt
+++ app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/MainActivity.kt
@@ -2,10 +2,12 @@ package com.yourssu.yds_ui_tester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ObservableBoolean
+import com.yourssu.design.system.atom.Picker
 import com.yourssu.design.system.component.Toast
 import com.yourssu.design.system.component.Toast.Companion.shortToast
 import com.yourssu.design.system.component.Toast.Companion.toast
@@ -20,12 +22,24 @@ class MainActivity : AppCompatActivity() {
     var triggerTextStyle = ObservableBoolean(false)
 
     private val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28)
+    val listString = listOf("00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55")
+    val listString2 = listOf("오전", "오후")
+
+    val onValueChangeListener = object : Picker.OnValueChangeListener {
+        override fun onValueChange(
+            firstValue: String,
+            secondValue: String,
+            thirdValue: String,
+            totalValue: String
+        ) {
+            Log.d("aaaa", "$firstValue, $secondValue, $thirdValue, $totalValue")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.activity = this
-
         binding.button.setOnClickListener {
             triggerTextStyle.set(!triggerTextStyle.get())
             bottomSheet {
