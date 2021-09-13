e13aafe53128f2b5c8f71b53af5228a343c3db2d
junghyun park <parkmint.urssu@gmail.com>
2021-09-14 01:26:12 +0900

    [improve] badge 리팩토링 및 바인딩 어뎁터 형식으로 변경

--- app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/BadgeActivity.kt
+++ app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/BadgeActivity.kt
@@ -5,11 +5,12 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.yourssu.design.system.atom.Badge
 import com.yourssu.design.system.foundation.Icon
+import com.yourssu.design_model.ItemColor
 import com.yourssu.yds_ui_tester.databinding.ActivityBadgeBinding
 
 class BadgeActivity : AppCompatActivity() {
     lateinit var binding: ActivityBadgeBinding
-    var colorData: Int = Badge.INDIGO
+    var colorData: ItemColor = ItemColor.Indigo
     var iconData: Int = Icon.ic_playcircle_filled
     var textData: String = "데이터바인딩"
 
