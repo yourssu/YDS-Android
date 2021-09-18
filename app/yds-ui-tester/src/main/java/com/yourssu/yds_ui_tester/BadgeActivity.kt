18b3f1c14f8dbea70a085d4e97d6fe71ae7819e8
junghyun park <parkmint.urssu@gmail.com>
2021-09-18 14:27:44 +0900

    jitpack bug 모듈별 참조 불가능으로 인한 수정

--- app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/BadgeActivity.kt
+++ app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/BadgeActivity.kt
@@ -3,9 +3,8 @@ package com.yourssu.yds_ui_tester
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.yourssu.design.system.atom.Badge
 import com.yourssu.design.system.foundation.Icon
-import com.yourssu.design_model.ItemColor
+import com.yourssu.design.system.foundation.ItemColor
 import com.yourssu.yds_ui_tester.databinding.ActivityBadgeBinding
 
 class BadgeActivity : AppCompatActivity() {
