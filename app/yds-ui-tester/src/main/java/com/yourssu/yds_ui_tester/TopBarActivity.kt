80d702bda84e1bba402bd1bb05c2e13e9e321a43
Gael-Android <gael.urssu@gmail.com>
2021-09-30 22:18:44 +0900

    싱글타이틀탑바 구현 어느정도 완료( 탑바버튼이 없어서 플레인버튼으로 대체)
    싱글타이틀바를 컨스트레인트로 구현하였음.
    탑바버튼이 없어서 일단 플레인버튼으로 구현함.

---
+++ app/yds-ui-tester/src/main/java/com/yourssu/yds_ui_tester/TopBarActivity.kt
@@ -0,0 +1,19 @@
+package com.yourssu.yds_ui_tester
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.yourssu.yds_ui_tester.databinding.ActivityTopBarBinding
+
+class TopBarActivity : AppCompatActivity() {
+    lateinit var binding: ActivityTopBarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_top_bar
+        )
+    }
+}
