4b04cd2b24ce15c281a54ab34ab36c3d04d7270c
Gael-Android <gael.urssu@gmail.com>
2021-11-10 15:51:40 +0900

    버튼에 텍스트 테스트완료, 버튼 없앨 때 자리 차지 X 확인.

--- DesignSystem/src/main/java/com/yourssu/design/system/component/SingleTitleTopBar.kt
+++ DesignSystem/src/main/java/com/yourssu/design/system/component/SingleTitleTopBar.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import com.yourssu.design.databinding.LayoutSingleTitleTopBarBinding
+import com.yourssu.design.system.foundation.Icon
 
 class SingleTitleTopBar @JvmOverloads constructor(
     context: Context,
@@ -18,18 +19,135 @@ class SingleTitleTopBar @JvmOverloads constructor(
     var title: String = ""
         set(title) {
             field = title
-            setTitleText()
+            binding.title.text = title
         }
 
-    private fun setTitleText() {
-        binding.title.text = title
-    }
+    var firstButtonListener: OnClickListener? = null
+        set(clickListener) {
+            field = clickListener
+            binding.firstButton.setOnClickListener(firstButtonListener)
+        }
+
+    var secondButtonListener: OnClickListener? = null
+        set(clickListener) {
+            field = clickListener
+            binding.secondButton.setOnClickListener(secondButtonListener)
+        }
+
+    var thirdButtonListener: OnClickListener? = null
+        set(clickListener) {
+            field = clickListener
+            binding.thirdButton.setOnClickListener(thirdButtonListener)
+        }
+
+    @Icon.Iconography
+    var firstIcon: Int? = null
+        set(icon) {
+            field = icon
+            binding.firstButton.icon = icon
+        }
+
+    @Icon.Iconography
+    var secondIcon: Int? = null
+        set(icon) {
+            field = icon
+            binding.secondButton.icon = icon
+        }
+
+    @Icon.Iconography
+    var thirdIcon: Int? = null
+        set(icon) {
+            field = icon
+            binding.thirdButton.icon = icon
+        }
+
+    var firstText: String = ""
+        set(value) {
+            field = value
+            binding.firstButton.text = value
+        }
+
+    var secondText: String = ""
+        set(value) {
+            field = value
+            binding.secondButton.text = value
+        }
+
+    var thirdText: String = ""
+        set(value) {
+            field = value
+            binding.thirdButton.text = value
+        }
 
     companion object {
         @JvmStatic
         @BindingAdapter("title")
         fun setTitle(singleTitleTopBar: SingleTitleTopBar, title: String) {
             singleTitleTopBar.title = title
+        }
+        
+        @JvmStatic
+        @BindingAdapter("firstIcon")
+        fun setFirstIcon(singleTitleTopBar: SingleTitleTopBar, @Icon.Iconography icon: Int?) {
+            singleTitleTopBar.firstIcon = icon
+        }
+
+        @JvmStatic
+        @BindingAdapter("secondIcon")
+        fun setSecondIcon(singleTitleTopBar: SingleTitleTopBar, @Icon.Iconography icon: Int?) {
+            singleTitleTopBar.secondIcon = icon
+        }
+
+        @JvmStatic
+        @BindingAdapter("thirdIcon")
+        fun setThirdIcon(singleTitleTopBar: SingleTitleTopBar, @Icon.Iconography icon: Int?) {
+            singleTitleTopBar.thirdIcon = icon
+        }
+
+        @JvmStatic
+        @BindingAdapter("firstText")
+        fun setFirstText(singleTitleTopBar: SingleTitleTopBar, value: String) {
+            singleTitleTopBar.firstText = value
+        }
+
+        @JvmStatic
+        @BindingAdapter("secondText")
+        fun setSecondText(singleTitleTopBar: SingleTitleTopBar, value: String) {
+            singleTitleTopBar.secondText = value
+        }
+
+        @JvmStatic
+        @BindingAdapter("thirdText")
+        fun setThirdText(singleTitleTopBar: SingleTitleTopBar, value: String) {
+            singleTitleTopBar.thirdText = value
+        }
+
+
+        @JvmStatic
+        @BindingAdapter("firstButtonClickListener")
+        fun setFirstButtonClickListener(
+            singleTitleTopBar: SingleTitleTopBar,
+            onClickListener: OnClickListener
+        ) {
+            singleTitleTopBar.firstButtonListener = onClickListener
+        }
+
+        @JvmStatic
+        @BindingAdapter("secondButtonClickListener")
+        fun setSecondButtonClickListener(
+            singleTitleTopBar: SingleTitleTopBar,
+            onClickListener: OnClickListener
+        ) {
+            singleTitleTopBar.secondButtonListener = onClickListener
+        }
+
+        @JvmStatic
+        @BindingAdapter("thirdButtonClickListener")
+        fun setThirdButtonClickListener(
+            singleTitleTopBar: SingleTitleTopBar,
+            onClickListener: OnClickListener
+        ) {
+            singleTitleTopBar.thirdButtonListener = onClickListener
         }
     }
 }
