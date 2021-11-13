52fe931fb0f0763075fbc1974c88ad0a8a6d14f9
Gael-Android <gael.urssu@gmail.com>
2021-11-13 17:37:59 +0900

    Revert "Merge branch 'develop' into feature/gael/topbar"
    This reverts commit 850b82f39377ad628f8a3585692008d9ce69e696, reversing
    changes made to b15d09f584b6afb0a7317ea8f7edb21ead9853df.

--- DesignSystem/src/main/java/com/yourssu/design/system/component/DoubleTitleTopBar.kt
+++ DesignSystem/src/main/java/com/yourssu/design/system/component/DoubleTitleTopBar.kt
@@ -2,12 +2,11 @@ package com.yourssu.design.system.component
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import com.yourssu.design.databinding.LayoutDoubleTitleTopBarBinding
+import com.yourssu.design.system.atom.Badge
 import com.yourssu.design.system.foundation.Icon
 
 class DoubleTitleTopBar @JvmOverloads constructor(
@@ -17,12 +16,6 @@ class DoubleTitleTopBar @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
     private val binding: LayoutDoubleTitleTopBarBinding =
         LayoutDoubleTitleTopBarBinding.inflate(LayoutInflater.from(context), this, true)
-
-    init {
-        binding.firstButton.visibility = GONE
-        binding.secondButton.visibility = GONE
-        binding.thirdButton.visibility = GONE
-    }
 
     var title: String = ""
         set(title) {
@@ -54,33 +47,10 @@ class DoubleTitleTopBar @JvmOverloads constructor(
             binding.thirdButton.setOnClickListener(thirdButtonListener)
         }
 
-    var firstButtonVisibility: Int = GONE
-        set(value) {
-            field = value
-            binding.firstButton.visibility = value
-        }
-
-    var secondButtonVisibility: Int = GONE
-        set(value) {
-            field = value
-            binding.secondButton.visibility = value
-        }
-
-    var thirdButtonVisibility: Int = GONE
-        set(value) {
-            field = value
-            binding.thirdButton.visibility = value
-        }
-
     @Icon.Iconography
     var firstIcon: Int? = null
         set(icon) {
             field = icon
-            firstButtonVisibility = if (firstText == "" && firstIcon == null) {
-                GONE
-            } else {
-                VISIBLE
-            }
             binding.firstButton.icon = icon
         }
 
@@ -88,11 +58,6 @@ class DoubleTitleTopBar @JvmOverloads constructor(
     var secondIcon: Int? = null
         set(icon) {
             field = icon
-            secondButtonVisibility = if (secondText == "" && secondIcon == null) {
-                GONE
-            } else {
-                VISIBLE
-            }
             binding.secondButton.icon = icon
         }
 
@@ -100,46 +65,7 @@ class DoubleTitleTopBar @JvmOverloads constructor(
     var thirdIcon: Int? = null
         set(icon) {
             field = icon
-            thirdButtonVisibility = if (thirdText == "" && thirdIcon == null) {
-                GONE
-            } else {
-                VISIBLE
-            }
             binding.thirdButton.icon = icon
-        }
-
-    var firstText: String = ""
-        set(value) {
-            field = value
-            firstButtonVisibility = if (firstText == "" && firstIcon == null) {
-                GONE
-            } else {
-                VISIBLE
-
-            }
-            binding.firstButton.text = value
-        }
-
-    var secondText: String = ""
-        set(value) {
-            field = value
-            secondButtonVisibility = if (secondText == "" && secondIcon == null) {
-                GONE
-            } else {
-                VISIBLE
-            }
-            binding.secondButton.text = value
-        }
-
-    var thirdText: String = ""
-        set(value) {
-            field = value
-            thirdButtonVisibility = if (thirdText == "" && thirdIcon == null) {
-                GONE
-            } else {
-                VISIBLE
-            }
-            binding.thirdButton.text = value
         }
 
     companion object {
@@ -174,47 +100,10 @@ class DoubleTitleTopBar @JvmOverloads constructor(
         }
 
         @JvmStatic
-        @BindingAdapter("firstText")
-        fun setFirstText(doubleTitleTopBar: DoubleTitleTopBar, value: String) {
-            doubleTitleTopBar.firstText = value
-        }
-
-        @JvmStatic
-        @BindingAdapter("secondText")
-        fun setSecondText(doubleTitleTopBar: DoubleTitleTopBar, value: String) {
-            doubleTitleTopBar.secondText = value
-        }
-
-        @JvmStatic
-        @BindingAdapter("thirdText")
-        fun setThirdText(doubleTitleTopBar: DoubleTitleTopBar, value: String) {
-            doubleTitleTopBar.thirdText = value
-        }
-
-        @JvmStatic
-        @BindingAdapter("firstButtonVisibility")
-        fun setFirstButtonVisibility(doubleTitleTopBar: DoubleTitleTopBar, state: Int) {
-            doubleTitleTopBar.firstButtonVisibility = state
-        }
-
-        @JvmStatic
-        @BindingAdapter("secondButtonVisibility")
-        fun setSecondButtonVisibility(doubleTitleTopBar: DoubleTitleTopBar, state: Int) {
-            doubleTitleTopBar.secondButtonVisibility = state
-        }
-
-        @JvmStatic
-        @BindingAdapter("thirdButtonVisibility")
-        fun setThirdButtonVisibility(doubleTitleTopBar: DoubleTitleTopBar, state: Int) {
-            doubleTitleTopBar.thirdButtonVisibility = state
-        }
-
-
-        @JvmStatic
         @BindingAdapter("firstButtonClickListener")
         fun setFirstButtonClickListener(
             doubleTitleTopBar: DoubleTitleTopBar,
-            onClickListener: OnClickListener
+            onClickListener:OnClickListener
         ) {
             doubleTitleTopBar.firstButtonListener = onClickListener
         }
@@ -223,7 +112,7 @@ class DoubleTitleTopBar @JvmOverloads constructor(
         @BindingAdapter("secondButtonClickListener")
         fun setSecondButtonClickListener(
             doubleTitleTopBar: DoubleTitleTopBar,
-            onClickListener: OnClickListener
+            onClickListener:OnClickListener
         ) {
             doubleTitleTopBar.secondButtonListener = onClickListener
         }
@@ -232,7 +121,7 @@ class DoubleTitleTopBar @JvmOverloads constructor(
         @BindingAdapter("thirdButtonClickListener")
         fun setThirdButtonClickListener(
             doubleTitleTopBar: DoubleTitleTopBar,
-            onClickListener: OnClickListener
+            onClickListener:OnClickListener
         ) {
             doubleTitleTopBar.thirdButtonListener = onClickListener
         }
