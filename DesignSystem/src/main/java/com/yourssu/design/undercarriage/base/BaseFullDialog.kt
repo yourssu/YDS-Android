5865e814ed67d286fd7c682027f48dc1d71db21b
ByeongsuPark <byeongsu@soongsil.ac.kr>
2022-02-11 20:47:29 +0900

    [Bugfix] 바텀시트 잔상 현상 수정

--- DesignSystem/src/main/java/com/yourssu/design/undercarriage/base/BaseFullDialog.kt
+++ DesignSystem/src/main/java/com/yourssu/design/undercarriage/base/BaseFullDialog.kt
@@ -29,15 +29,12 @@ abstract class BaseFullDialog: Dialog {
     private fun setFullDialog(isAttachWindow: Boolean = false) {
         if (isAttachWindow && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window?.setDecorFitsSystemWindows(false)
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             val insetController: WindowInsetsController? = window?.insetsController
             // insetController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
             insetController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_BARS_BY_SWIPE
         } else if (!isAttachWindow && Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
             window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
             window?.requestFeature(Window.FEATURE_NO_TITLE)
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
             window?.decorView?.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         }
