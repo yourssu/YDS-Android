fbf845758ccad4f767f024b5df5db5719d6fbf5d
ByeongsuPark <byeongsu@soongsil.ac.kr>
2022-03-02 16:09:44 +0900

    [YDS] 아이콘 수정 및 추가

--- DesignSystem/src/main/java/com/yourssu/design/system/foundation/Iconography.kt
+++ DesignSystem/src/main/java/com/yourssu/design/system/foundation/Iconography.kt
@@ -34,6 +34,10 @@ object Icon {
         ic_dotbadge_line,
         ic_dots_horizontal_line,
         ic_dots_vertical_line,
+        ic_download_line,
+        ic_emojiadd_line,
+        ic_eyeclosed_line,
+        ic_eyeopen_line,
         ic_food_filled,
         ic_food_line,
         ic_foodcalendar_filled,
@@ -84,7 +88,8 @@ object Icon {
         ic_trashcan_line,
         ic_warningcircle_filled,
         ic_warningcircle_line,
-        ic_x_line
+        ic_x_line,
+        ic_xcircle_filled
     ])
     annotation class Iconography
 
@@ -115,57 +120,62 @@ object Icon {
     const val ic_dotbadge_line = 24
     const val ic_dots_horizontal_line = 25
     const val ic_dots_vertical_line = 26
-    const val ic_food_filled = 27
-    const val ic_food_line = 28
-    const val ic_foodcalendar_filled = 29
-    const val ic_foodcalendar_line = 30
-    const val ic_ground_filled = 31
-    const val ic_ground_line = 32
-    const val ic_home_filled = 33
-    const val ic_home_line = 34
-    const val ic_list_line = 35
-    const val ic_lock_filled = 36
-    const val ic_lock_line = 37
-    const val ic_new_filled = 38
-    const val ic_new_line = 39
-    const val ic_notice_filled = 40
-    const val ic_notice_line = 41
-    const val ic_pen_filled = 42
-    const val ic_pen_line = 43
-    const val ic_person_filled = 44
-    const val ic_person_line = 45
-    const val ic_personcircle_line = 46
-    const val ic_picture_filled = 47
-    const val ic_picture_line = 48
-    const val ic_pin_filled = 49
-    const val ic_pin_line = 50
-    const val ic_playcircle_filled = 51
-    const val ic_playcircle_line = 52
-    const val ic_plus_line = 53
-    const val ic_rank_filled = 54
-    const val ic_rank_line = 55
-    const val ic_recomment_line = 56
-    const val ic_refresh_line = 57
-    const val ic_savecircle_filled = 58
-    const val ic_savecircle_line = 59
-    const val ic_schoolcalendar_filled = 60
-    const val ic_schoolcalendar_line = 61
-    const val ic_search_line = 62
-    const val ic_sharecircle_filled = 63
-    const val ic_sharecircle_line = 64
-    const val ic_star_filled = 65
-    const val ic_star_line = 66
-    const val ic_thumb_down_filled = 67
-    const val ic_thumb_down_line = 68
-    const val ic_thumb_up_filled = 69
-    const val ic_thumb_up_line = 70
-    const val ic_timecalendar_filled = 71
-    const val ic_timecalendar_line = 72
-    const val ic_trashcan_filled = 73
-    const val ic_trashcan_line = 74
-    const val ic_warningcircle_filled = 75
-    const val ic_warningcircle_line = 76
-    const val ic_x_line = 77
+    const val ic_download_line = 27
+    const val ic_emojiadd_line = 28
+    const val ic_eyeclosed_line = 29
+    const val ic_eyeopen_line = 30
+    const val ic_food_filled = 31
+    const val ic_food_line = 32
+    const val ic_foodcalendar_filled = 33
+    const val ic_foodcalendar_line = 34
+    const val ic_ground_filled = 35
+    const val ic_ground_line = 36
+    const val ic_home_filled = 37
+    const val ic_home_line = 38
+    const val ic_list_line = 39
+    const val ic_lock_filled = 40
+    const val ic_lock_line = 41
+    const val ic_new_filled = 42
+    const val ic_new_line = 43
+    const val ic_notice_filled = 44
+    const val ic_notice_line = 45
+    const val ic_pen_filled = 46
+    const val ic_pen_line = 47
+    const val ic_person_filled = 48
+    const val ic_person_line = 49
+    const val ic_personcircle_line = 50
+    const val ic_picture_filled = 51
+    const val ic_picture_line = 52
+    const val ic_pin_filled = 53
+    const val ic_pin_line = 54
+    const val ic_playcircle_filled = 55
+    const val ic_playcircle_line = 56
+    const val ic_plus_line = 57
+    const val ic_rank_filled = 58
+    const val ic_rank_line = 59
+    const val ic_recomment_line = 60
+    const val ic_refresh_line = 61
+    const val ic_savecircle_filled = 62
+    const val ic_savecircle_line = 63
+    const val ic_schoolcalendar_filled = 64
+    const val ic_schoolcalendar_line = 65
+    const val ic_search_line = 66
+    const val ic_sharecircle_filled = 67
+    const val ic_sharecircle_line = 68
+    const val ic_star_filled = 69
+    const val ic_star_line = 70
+    const val ic_thumb_down_filled = 71
+    const val ic_thumb_down_line = 72
+    const val ic_thumb_up_filled = 73
+    const val ic_thumb_up_line = 74
+    const val ic_timecalendar_filled = 75
+    const val ic_timecalendar_line = 76
+    const val ic_trashcan_filled = 77
+    const val ic_trashcan_line = 78
+    const val ic_warningcircle_filled = 79
+    const val ic_warningcircle_line = 80
+    const val ic_x_line = 81
+    const val ic_xcircle_filled= 82
 
     @DrawableRes
     fun getIconDrawable(@Iconography icon: Int): Int {
@@ -197,6 +207,10 @@ object Icon {
             ic_dotbadge_line -> R.drawable.ic_dotbadge_line
             ic_dots_horizontal_line -> R.drawable.ic_dots_horizontal_line
             ic_dots_vertical_line -> R.drawable.ic_dots_vertical_line
+            ic_download_line -> R.drawable.ic_download_line
+            ic_emojiadd_line -> R.drawable.ic_emojiadd_line
+            ic_eyeclosed_line -> R.drawable.ic_eyeclosed_line
+            ic_eyeopen_line -> R.drawable.ic_eyeopen_line
             ic_food_filled -> R.drawable.ic_food_filled
             ic_food_line -> R.drawable.ic_food_line
             ic_foodcalendar_filled -> R.drawable.ic_foodcalendar_filled
@@ -248,6 +262,7 @@ object Icon {
             ic_warningcircle_filled -> R.drawable.ic_warningcircle_filled
             ic_warningcircle_line -> R.drawable.ic_warningcircle_line
             ic_x_line -> R.drawable.ic_x_line
+            ic_xcircle_filled -> R.drawable.ic_xcircle_filled
             else -> R.drawable.ic_adbadge_filled
         }
     }
@@ -281,6 +296,10 @@ object Icon {
             ic_dotbadge_line -> "ic_dotbadge_line"
             ic_dots_horizontal_line -> "ic_dots_horizontal_line"
             ic_dots_vertical_line -> "ic_dots_vertical_line"
+            ic_download_line -> "ic_download_line"
+            ic_emojiadd_line -> "ic_emojiadd_line"
+            ic_eyeclosed_line -> "ic_eyeclosed_line"
+            ic_eyeopen_line -> "ic_eyeopen_line"
             ic_food_filled -> "ic_food_filled"
             ic_food_line -> "ic_food_line"
             ic_foodcalendar_filled -> "ic_foodcalendar_filled"
@@ -332,6 +351,7 @@ object Icon {
             ic_warningcircle_filled -> "ic_warningcircle_filled"
             ic_warningcircle_line -> "ic_warningcircle_line"
             ic_x_line -> "ic_x_line"
+            ic_xcircle_filled -> "ic_xcircle_filled"
             else -> "ic_adbadge_filled"
         }
     }
@@ -365,6 +385,10 @@ object Icon {
             "ic_dotbadge_line" -> ic_dotbadge_line
             "ic_dots_horizontal_line" -> ic_dots_horizontal_line
             "ic_dots_vertical_line" -> ic_dots_vertical_line
+            "ic_download_line" -> ic_download_line
+            "ic_emojiadd_line" -> ic_emojiadd_line
+            "ic_eyeclosed_line" -> ic_eyeclosed_line
+            "ic_eyeopen_line" -> ic_eyeopen_line
             "ic_food_filled" -> ic_food_filled
             "ic_food_line" -> ic_food_line
             "ic_foodcalendar_filled" -> ic_foodcalendar_filled
@@ -416,6 +440,7 @@ object Icon {
             "ic_warningcircle_filled" -> ic_warningcircle_filled
             "ic_warningcircle_line" -> ic_warningcircle_line
             "ic_x_line" -> ic_x_line
+            "ic_xcircle_filled" -> ic_xcircle_filled
             else -> ic_adbadge_filled
         }
     }
@@ -449,6 +474,10 @@ object Icon {
             ic_dotbadge_line,
             ic_dots_horizontal_line,
             ic_dots_vertical_line,
+            ic_download_line,
+            ic_emojiadd_line,
+            ic_eyeclosed_line,
+            ic_eyeopen_line,
             ic_food_filled,
             ic_food_line,
             ic_foodcalendar_filled,
@@ -499,7 +528,8 @@ object Icon {
             ic_trashcan_line,
             ic_warningcircle_filled,
             ic_warningcircle_line,
-            ic_x_line
+            ic_x_line,
+            ic_xcircle_filled
         )
     }
 }
