e69cc16a9e8deaf5bc691730922ed161c3cdeafb
ByeongsuPark <byeongsu@soongsil.ac.kr>
2022-05-11 22:47:40 +0900

    [Bugfix] BoxButton pressed 상태 반영되지 않는 버그 수정

--- DesignSystem/src/main/java/com/yourssu/design/system/atom/BoxButton.kt
+++ DesignSystem/src/main/java/com/yourssu/design/system/atom/BoxButton.kt
@@ -280,16 +280,16 @@ class BoxButton : LinearLayout {
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+        super.onTouchEvent(event)
         if (event != null) {
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     isPressed = true
                     setBoxButtonInfo()
                 }
-                MotionEvent.ACTION_UP -> {
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
                     isPressed = false
                     setBoxButtonInfo()
-                    performClick() // 손을 떼는 순간만 인식해야함
                 }
             }
         }
