ca58bd634ac8a8e0777343052dbeb302c0433653
정해성 <harryjung1001@gmail.com>
2022-09-06 01:02:42 +0900

    [YC-6] ListItem pressed color, state saver 구현

--- ComposeDesignSystem/src/main/java/com/yourssu/composedesignsystem/ui/theme/atom/ListItem.kt
+++ ComposeDesignSystem/src/main/java/com/yourssu/composedesignsystem/ui/theme/atom/ListItem.kt
@@ -2,13 +2,17 @@ package com.yourssu.composedesignsystem.ui.theme.atom
 
 import android.util.Log
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yourssu.composedesignsystem.R
@@ -28,8 +32,43 @@ data class ListItemState(
     var rightIconState by mutableStateOf(rightIcon)
     var isDisabledState by mutableStateOf(isDisabled)
 
-    val isPressed: Boolean
+    private val isPressed: Boolean
         @Composable get() = interactionSource.collectIsPressedAsState().value
+
+    val backgroundColor: Color
+        @Composable get() = if (isPressed && !isDisabledState) {
+            YdsTheme.colors.bgPressed
+        } else {
+            YdsTheme.colors.bgNormal
+        }
+
+    companion object {
+        val Saver = run {
+            val textKey = "text"
+            val leftIconKey = "leftIcon"
+            val rightIconKey = "rightIcon"
+            val disabledKey = "disabled"
+
+            mapSaver(
+                save = {
+                    mapOf(
+                        textKey to it.textState,
+                        leftIconKey to it.leftIconState,
+                        rightIconKey to it.rightIconState,
+                        disabledKey to it.isDisabledState
+                    )
+                },
+                restore = {
+                    ListItemState(
+                        it[textKey] as String,
+                        it[leftIconKey] as Int?,
+                        it[rightIconKey] as Int?,
+                        it[disabledKey] as Boolean
+                    )
+                }
+            )
+        }
+    }
 }
 
 @Composable
@@ -39,8 +78,9 @@ fun rememberListItemState(
     @DrawableRes rightIcon: Int? = null,
     isDisabled: Boolean = false,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
-): ListItemState = remember(
-    text, leftIcon, rightIcon, isDisabled, interactionSource
+): ListItemState = rememberSaveable(
+    text, leftIcon, rightIcon, isDisabled, interactionSource,
+    saver = ListItemState.Saver
 ) {
     ListItemState(text, leftIcon, rightIcon, isDisabled, interactionSource)
 }
@@ -56,44 +96,48 @@ fun ListItem(
             .then(modifier)
             .fillMaxWidth()
             .height(48.dp)
-            .noRippleClickable {
-                onClick()
-            },
+            .noRippleClickable(interactionSource = state.interactionSource) {
+                if (!state.isDisabledState) {
+                    onClick()
+                }
+            }
+            .background(color = state.backgroundColor),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
+        Spacer(Modifier.width(20.dp))
+
         state.leftIconState?.let { leftIconId ->
-            Spacer(Modifier.width(20.dp))
             YdsIcon(id = leftIconId)
+            Spacer(Modifier.width(8.dp))
         }
 
-        Spacer(Modifier.width(8.dp))
         Text(
             text = state.textState,
             modifier = Modifier.weight(1.0f),
             style = YdsTheme.typography.body1
         )
-        Spacer(Modifier.width(8.dp))
 
         state.rightIconState?.let { rightIconId ->
+            Spacer(Modifier.width(8.dp))
             YdsIcon(id = rightIconId)
-            Spacer(Modifier.width(20.dp))
         }
+
+        Spacer(Modifier.width(20.dp))
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ListItemPreview() {
-    var count by remember {
-        mutableStateOf(0)
-    }
+    var count by remember { mutableStateOf(0) }
+    val listItemState = rememberListItemState(
+        text = "로그아웃",
+        rightIcon = R.drawable.ic_ground_filled
+    )
+
     ListItem(
-        onClick = { Log.d("asdf", "ListItemPreview: ${count++}") },
-        state = rememberListItemState(
-            text = "로그아웃",
-            isDisabled = true,
-            leftIcon = R.drawable.ic_ground_filled
-        )
+        onClick = { Log.d("test", "ListItemPreview: ${count++}") },
+        state = listItemState
     )
 }
