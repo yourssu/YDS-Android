697dfe7c4c083203b772f074493b3feca9c9951a
정해성 <harryjung1001@gmail.com>
2023-08-26 21:05:34 +0900

    refactor: YdsTypography

--- compose/src/main/java/com/yourssu/design/system/compose/YdsTheme.kt
+++ compose/src/main/java/com/yourssu/design/system/compose/YdsTheme.kt
@@ -1,27 +1,22 @@
 package com.yourssu.design.system.compose
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.Shapes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import com.yourssu.design.system.compose.foundation.LocalYdsColorScheme
+import com.yourssu.design.system.compose.foundation.LocalYdsRounding
 import com.yourssu.design.system.compose.foundation.LocalYdsTypography
-import com.yourssu.design.system.compose.rule.Border
-import com.yourssu.design.system.compose.rule.LocalYdsBorder
 import com.yourssu.design.system.compose.foundation.YdsColorScheme
+import com.yourssu.design.system.compose.foundation.YdsRounding
 import com.yourssu.design.system.compose.foundation.YdsTypography
 import com.yourssu.design.system.compose.foundation.darkColorScheme
-import com.yourssu.design.system.compose.foundation.getYDSTypography
 import com.yourssu.design.system.compose.foundation.lightColorScheme
-import com.yourssu.design.system.compose.foundation.LocalYdsRounding
-import com.yourssu.design.system.compose.foundation.YdsRounding
 
 @Composable
 fun YdsTheme(
-    typography: YdsTypography = getYDSTypography(),
+    typography: YdsTypography = YdsTheme.typography,
     rounding: YdsRounding = YdsTheme.rounding,
-    border: Border = YdsTheme.border,
     content: @Composable () -> Unit
 ) {
     val colors = if (isSystemInDarkTheme()) {
@@ -33,7 +28,6 @@ fun YdsTheme(
         LocalYdsColorScheme provides colors,
         LocalYdsTypography provides typography,
         LocalYdsRounding provides rounding,
-        LocalYdsBorder provides border,
         content = content
     )
 }
@@ -53,9 +47,4 @@ object YdsTheme {
         @Composable
         @ReadOnlyComposable
         get() = LocalYdsRounding.current
-
-    val border: Border
-        @Composable
-        @ReadOnlyComposable
-        get() = LocalYdsBorder.current
 }
--- compose/src/main/java/com/yourssu/design/system/compose/foundation/Typography.kt
+++ compose/src/main/java/com/yourssu/design/system/compose/foundation/Typography.kt
@@ -9,154 +9,143 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.yourssu.design.system.compose.R
 
 val fonts = FontFamily(
     Font(R.font.spoqa_han_sans_neo_regular, FontWeight.Normal),
     Font(R.font.spoqa_han_sans_neo_medium, FontWeight.Medium),
-    Font(R.font.spoqa_han_sans_neo_bold, FontWeight.Bold)
+    Font(R.font.spoqa_han_sans_neo_bold, FontWeight.Bold),
 )
+
+// 폰트 사이즈를 Dp로 표현하기 위해 사용합니다.
+// Text에 적용할 때는 `toTextStyle()`로 변환하여 사용합니다.
+@Immutable
+data class YdsTextStyle(
+    val fontFamily: FontFamily,
+    val fontWeight: FontWeight,
+    val fontSize: Dp,
+    val lineHeight: Dp,
+) {
+    @Composable
+    fun toTextStyle() = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = fontWeight,
+        fontSize = with(LocalDensity.current) { fontSize.toSp() },
+        lineHeight = with(LocalDensity.current) { lineHeight.toSp() },
+    )
+}
 
 @Immutable
 data class YdsTypography(
-    val title1: TextStyle = TextStyle.Default,
-    val title2: TextStyle = TextStyle.Default,
-    val title3: TextStyle = TextStyle.Default,
-    val subTitle1: TextStyle = TextStyle.Default,
-    val subTitle2: TextStyle = TextStyle.Default,
-    val subTitle3: TextStyle = TextStyle.Default,
-    val body1: TextStyle = TextStyle.Default,
-    val body2: TextStyle = TextStyle.Default,
-    val button0: TextStyle = TextStyle.Default,
-    val button1: TextStyle = TextStyle.Default,
-    val button2: TextStyle = TextStyle.Default,
-    val button3: TextStyle = TextStyle.Default,
-    val button4: TextStyle = TextStyle.Default,
-    val caption0: TextStyle = TextStyle.Default,
-    val caption1: TextStyle = TextStyle.Default,
-    val caption2: TextStyle = TextStyle.Default,
-    val display1: TextStyle = TextStyle.Default,
-    val display2: TextStyle = TextStyle.Default,
+    val title1: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.dp,
+        lineHeight = 36.4f.dp,
+    ),
+    val title2: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.dp,
+        lineHeight = 31.2f.dp,
+    ),
+    val title3: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.dp,
+        lineHeight = 26.dp,
+    ),
+    val subTitle1: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.dp,
+        lineHeight = 26.dp,
+    ),
+    val subTitle2: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.dp,
+        lineHeight = 20.8f.dp,
+    ),
+    val subTitle3: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.dp,
+        lineHeight = 18.2f.dp,
+    ),
+    val body1: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.dp,
+        lineHeight = 22.5f.dp,
+    ),
+    val body2: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.dp,
+        lineHeight = 21f.dp,
+    ),
+    val button0: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.dp,
+        lineHeight = 22.4f.dp,
+    ),
+    val button1: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.dp,
+        lineHeight = 22.4f.dp,
+    ),
+    val button2: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.dp,
+        lineHeight = 18.2f.dp,
+    ),
+    val button3: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.dp,
+        lineHeight = 18.2f.dp,
+    ),
+    val button4: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.dp,
+        lineHeight = 16.8f.dp,
+    ),
+    val caption0: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.dp,
+        lineHeight = 15.6f.dp,
+    ),
+    val caption1: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.dp,
+        lineHeight = 15.6f.dp,
+    ),
+    val caption2: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.dp,
+        lineHeight = 14.3f.dp,
+    ),
+    val display1: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.dp,
+        lineHeight = 52.dp,
+    ),
+    val display2: YdsTextStyle = YdsTextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.dp,
+        lineHeight = 41.6f.dp,
+    ),
 )
 
 internal val LocalYdsTypography = staticCompositionLocalOf { YdsTypography() }
-
-@Composable
-fun getYDSTypography() =
-    YdsTypography(
-        title1 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Bold,
-            fontSize = 28.dp(),
-            lineHeight = 36.4f.dp()
-        ),
-        title2 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.dp(),
-            lineHeight = 31.2f.dp()
-        ),
-        title3 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.dp(),
-            lineHeight = 26.dp()
-        ),
-        subTitle1 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 20.dp(),
-            lineHeight = 26.dp()
-        ),
-        subTitle2 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 16.dp(),
-            lineHeight = 20.8f.dp()
-        ),
-        subTitle3 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 14.dp(),
-            lineHeight = 18.2f.dp()
-        ),
-        body1 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Normal,
-            fontSize = 15.dp(),
-            lineHeight = 22.5f.dp()
-        ),
-        body2 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Normal,
-            fontSize = 14.dp(),
-            lineHeight = 21f.dp()
-        ),
-        button0 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 16.dp(),
-            lineHeight = 22.4f.dp()
-        ),
-        button1 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 16.dp(),
-            lineHeight = 22.4f.dp()
-        ),
-        button2 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 14.dp(),
-            lineHeight = 18.2f.dp()
-        ),
-        button3 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 14.dp(),
-            lineHeight = 18.2f.dp()
-        ),
-        button4 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 12.dp(),
-            lineHeight = 16.8f.dp()
-        ),
-        caption0 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 12.dp(),
-            lineHeight = 15.6f.dp()
-        ),
-        caption1 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 12.dp(),
-            lineHeight = 15.6f.dp()
-        ),
-        caption2 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 11.dp(),
-            lineHeight = 14.3f.dp()
-        ),
-        display1 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Bold,
-            fontSize = 40.dp(),
-            lineHeight = 52.dp()
-        ),
-        display2 = TextStyle(
-            fontFamily = fonts,
-            fontWeight = FontWeight.Bold,
-            fontSize = 32.dp(),
-            lineHeight = 41.6f.dp()
-        )
-    )
-
-
-@Composable
-fun Float.dp() = with(LocalDensity.current) { Dp(this@dp).toSp() }
-
-@Composable
-fun Int.dp() = with(LocalDensity.current) { Dp(this@dp.toFloat()).toSp() }
