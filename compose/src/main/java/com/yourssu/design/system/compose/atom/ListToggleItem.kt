d1db15c29227e06d53568d4625feb04342695109
정해성 <harryjung1001@gmail.com>
2023-08-28 16:02:07 +0900

    fix: 마이너 오류 해결

--- compose/src/main/java/com/yourssu/design/system/compose/atom/ListToggleItem.kt
+++ compose/src/main/java/com/yourssu/design/system/compose/atom/ListToggleItem.kt
@@ -1,14 +1,24 @@
 package com.yourssu.design.system.compose.atom
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yourssu.design.system.compose.YdsTheme
+import com.yourssu.design.system.compose.base.Text
 
 @Composable
 fun ListToggleItem(
