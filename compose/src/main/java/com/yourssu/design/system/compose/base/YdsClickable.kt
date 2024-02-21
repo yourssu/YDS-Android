39b383db2b60f5bf4638175c2065e1223fe3e38a
Jung Haesung <48352078+cometj03@users.noreply.github.com>
2024-02-21 22:39:41 +0900

    [YDS-#255] ydsClickable 개선 (#256)
    * improve: #255
    
    * docs: pr 템플릿 수정
    
    * chore: version update
    
    * docs: pr 템플릿 수정

--- compose/src/main/java/com/yourssu/design/system/compose/base/YdsClickable.kt
+++ compose/src/main/java/com/yourssu/design/system/compose/base/YdsClickable.kt
@@ -2,17 +2,30 @@ package com.yourssu.design.system.compose.base
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 
 /**
  * (전) noRippleClickable
  *
  * ripple 효과가 없는 clickable입니다.
  */
-inline fun Modifier.ydsClickable(
+fun Modifier.ydsClickable(
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+): Modifier = composed {
+    this.ydsClickable(
+        interactionSource = remember { MutableInteractionSource() },
+        enabled = enabled,
+        onClick = onClick,
+    )
+}
+
+fun Modifier.ydsClickable(
     interactionSource: MutableInteractionSource,
     enabled: Boolean = true,
-    crossinline onClick: () -> Unit,
+    onClick: () -> Unit,
 ): Modifier = this.clickable(
     interactionSource = interactionSource,
     indication = null,
