314be0de36fa9316f57eaa9530e271fe03fa8a62
kangyuri1114 <yuri3334@gmail.com>
2024-03-17 18:10:17 +0900

    add : Icon에 contentDescription 속성 추가

--- compose/src/main/java/com/yourssu/design/system/compose/base/Icon.kt
+++ compose/src/main/java/com/yourssu/design/system/compose/base/Icon.kt
@@ -14,6 +14,10 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.yourssu.design.system.compose.foundation.LocalYdsContentColor
@@ -29,40 +33,53 @@ sealed class IconSize(val value: Dp) {
 fun Icon(
     @DrawableRes id: Int,
     modifier: Modifier = Modifier,
+    contentDescription: String? = null,
     iconSize: IconSize = IconSize.Medium,
     tint: Color = LocalYdsContentColor.current,
 ) {
     Icon(
         imageVector = ImageVector.vectorResource(id = id),
         modifier = modifier,
+        contentDescription = contentDescription,
         iconSize = iconSize,
-        tint = tint
+        tint = tint,
     )
 }
 
 @Composable
 fun Icon(
     imageVector: ImageVector,
+    contentDescription: String?,
     modifier: Modifier = Modifier,
     iconSize: IconSize = IconSize.Medium,
     tint: Color = LocalYdsContentColor.current,
 ) {
     Icon(
         painter = rememberVectorPainter(imageVector),
+        contentDescription = contentDescription,
         modifier = modifier,
         iconSize = iconSize,
-        tint = tint
+        tint = tint,
     )
 }
 
 @Composable
 fun Icon(
     painter: Painter,
+    contentDescription: String?,
     modifier: Modifier = Modifier,
     iconSize: IconSize = IconSize.Medium,
     tint: Color = LocalYdsContentColor.current,
 ) {
     val colorFilter = ColorFilter.tint(tint)
+    val semantics = if (contentDescription != null) {
+        Modifier.semantics {
+            this.contentDescription = contentDescription
+            this.role = Role.Image
+        }
+    } else {
+        Modifier
+    }
     Box(
         modifier
             .toolingGraphicsLayer()
@@ -70,7 +87,7 @@ fun Icon(
             .paint(
                 painter = painter,
                 colorFilter = colorFilter,
-                contentScale = ContentScale.Fit
-            )
+                contentScale = ContentScale.Fit,
+            ).then(semantics),
     )
 }
