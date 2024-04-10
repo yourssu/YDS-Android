640432e09fe1587586d544e28108f7dfdb146346
LEE YOU BIN <youbeene3409@naver.com>
2024-04-10 15:06:01 +0900

    fix : TextField의 hintText 8dp로 수정

--- compose/src/main/java/com/yourssu/design/system/compose/atom/SimpleTextField.kt
+++ compose/src/main/java/com/yourssu/design/system/compose/atom/SimpleTextField.kt
@@ -88,7 +88,7 @@ fun SimpleTextField(
             Row(modifier = Modifier.padding(top = 8.dp)) {
                 Spacer(
                     modifier = Modifier
-                        .width(16.dp),
+                        .width(8.dp),
                 )
                 YdsText(
                     text = hintText,
