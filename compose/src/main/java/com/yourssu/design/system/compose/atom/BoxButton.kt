6d1fd7b4a95f9d41de7b0afc3f1e0a882d5c3b7b
kangyuri1114 <yuri3334@gmail.com>
2024-05-09 19:12:43 +0900

    chore : BoxButtonType customColor 추가

--- compose/src/main/java/com/yourssu/design/system/compose/atom/BoxButton.kt
+++ compose/src/main/java/com/yourssu/design/system/compose/atom/BoxButton.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -28,20 +29,23 @@ import com.yourssu.design.system.compose.states.ButtonSizeState
 enum class BoxButtonType {
     Filled,
     Tinted,
-    Line
+    Line,
+    CustomColorFilled,
+    CustomColorLine,
 }
 
 enum class BoxButtonSize {
     ExtraLarge,
     Large,
     Medium,
-    Small
+    Small,
 }
 
 @Composable
 private fun boxButtonColorByType(
     isWarned: Boolean,
-    type: BoxButtonType
+    type: BoxButtonType,
+    customColor: Color,
 ): ButtonColorState = when (type) {
     BoxButtonType.Filled -> ButtonColorState(
         contentColor = YdsTheme.colors.buttonBright,
@@ -52,6 +56,7 @@ private fun boxButtonColorByType(
         },
         disabledBgColor = YdsTheme.colors.buttonDisabledBG,
     )
+
     BoxButtonType.Tinted -> ButtonColorState(
         contentColor = when {
             isWarned -> YdsTheme.colors.buttonWarned
@@ -62,44 +67,66 @@ private fun boxButtonColorByType(
             isWarned -> YdsTheme.colors.buttonWarnedBG
             else -> YdsTheme.colors.buttonPointBG
         },
-        disabledBgColor = YdsTheme.colors.buttonDisabledBG
+        disabledBgColor = YdsTheme.colors.buttonDisabledBG,
     )
+
     BoxButtonType.Line -> ButtonColorState(
         contentColor = when {
             isWarned -> YdsTheme.colors.buttonWarned
             else -> YdsTheme.colors.buttonPoint
         },
-        disabledContentColor = YdsTheme.colors.buttonDisabled
+        disabledContentColor = YdsTheme.colors.buttonDisabled,
+    )
+
+    BoxButtonType.CustomColorFilled -> ButtonColorState(
+        contentColor = YdsTheme.colors.buttonBright,
+        disabledContentColor = YdsTheme.colors.buttonDisabled,
+        bgColor = when {
+            isWarned -> YdsTheme.colors.buttonWarned
+            else -> customColor
+        },
+        disabledBgColor = YdsTheme.colors.buttonDisabledBG,
+    )
+
+    BoxButtonType.CustomColorLine -> ButtonColorState(
+        contentColor = when {
+            isWarned -> YdsTheme.colors.buttonWarned
+            else -> customColor
+        },
+        disabledContentColor = YdsTheme.colors.buttonDisabled,
     )
 }
 
 @Composable
 private fun boxButtonSizeStateBySize(
-    size: BoxButtonSize
+    size: BoxButtonSize,
 ): ButtonSizeState = when (size) {
     BoxButtonSize.ExtraLarge -> ButtonSizeState(
         typo = YdsTheme.typography.button1,
         iconSize = IconSize.Medium,
         height = 56.dp,
-        horizontalPadding = 16.dp
+        horizontalPadding = 16.dp,
     )
+
     BoxButtonSize.Large -> ButtonSizeState(
         typo = YdsTheme.typography.button2,
         iconSize = IconSize.Medium,
         height = 48.dp,
-        horizontalPadding = 16.dp
+        horizontalPadding = 16.dp,
     )
+
     BoxButtonSize.Medium -> ButtonSizeState(
         typo = YdsTheme.typography.button2,
         iconSize = IconSize.Medium,
         height = 40.dp,
-        horizontalPadding = 12.dp
+        horizontalPadding = 12.dp,
     )
+
     BoxButtonSize.Small -> ButtonSizeState(
         typo = YdsTheme.typography.button4,
         iconSize = IconSize.Small,
         height = 32.dp,
-        horizontalPadding = 12.dp
+        horizontalPadding = 12.dp,
     )
 }
 
@@ -115,7 +142,8 @@ fun BoxButton(
     sizeType: BoxButtonSize = BoxButtonSize.Large,
     buttonType: BoxButtonType = BoxButtonType.Filled,
     rounding: Dp = 8.dp,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    customBoxColor: Color = YdsTheme.colors.buttonPoint,
 ) {
     val roundingDp = when (sizeType) {
         BoxButtonSize.ExtraLarge -> 8.dp
@@ -126,29 +154,29 @@ fun BoxButton(
 
     YdsBaseButton(
         onClick = onClick,
-        colors = boxButtonColorByType(isWarned = isWarned, type = buttonType),
+        colors = boxButtonColorByType(isWarned = isWarned, type = buttonType, customColor = customBoxColor),
         modifier = Modifier
             .then(modifier)
             .height(height),
         enabled = !isDisabled,
-        showBorder = (buttonType == BoxButtonType.Line),
+        showBorder = (buttonType == BoxButtonType.Line || buttonType == BoxButtonType.CustomColorLine),
         interactionSource = interactionSource,
         rounding = roundingDp,
         contentPadding = PaddingValues(
-            horizontal = horizontalPadding
-        )
+            horizontal = horizontalPadding,
+        ),
     ) {
         leftIcon?.let { icon ->
             Icon(
                 id = icon,
-                iconSize = iconSize
+                iconSize = iconSize,
             )
             Spacer(modifier = Modifier.width(4.dp))
         }
 
         YdsText(
             text = text,
-            style = typo
+            style = typo,
         )
 
         // leftIcon이 null일 때만
@@ -156,7 +184,7 @@ fun BoxButton(
             Spacer(modifier = Modifier.width(4.dp))
             Icon(
                 id = icon,
-                iconSize = iconSize
+                iconSize = iconSize,
             )
         }
     }
@@ -169,19 +197,56 @@ private fun BoxButtonPreview() {
 
     YdsTheme {
         Column {
-            BoxButton(
-                onClick = { /*TODO*/ },
-                text = text,
-                rightIcon = R.drawable.ic_ground_filled,
-                buttonType = BoxButtonType.Line,
-                sizeType = BoxButtonSize.Small
-            )
+//            BoxButton(
+//                onClick = { /*TODO*/ },
+//                text = text,
+//                rightIcon = R.drawable.ic_ground_filled,
+//                buttonType = BoxButtonType.Line,
+//                sizeType = BoxButtonSize.Small,
+//            )
             BoxButton(
                 onClick = { text += "+" },
                 text = "BoxButton 2",
                 leftIcon = R.drawable.ic_ground_filled,
                 rightIcon = R.drawable.ic_ground_filled,
-                isWarned = true
+                isWarned = true,
+            )
+
+            // 기본
+            BoxButton(
+                onClick = { text += "+" },
+                text = "BoxButton 2",
+                leftIcon = R.drawable.ic_ground_filled,
+            )
+
+            // 커스텀 색 (기존 Fill + custom색상 -> 기존 fill이면 custom 색상 적용 안 됨)
+            BoxButton(
+                onClick = { text += "+" },
+                text = "BoxButton 2",
+                leftIcon = R.drawable.ic_ground_filled,
+                customBoxColor = YdsTheme.colors.logoLightBlue,
+            )
+
+            // 커스텀 색 지정 (buttonType이 CustomColorFilled이면 custom 색상 적용)
+            BoxButton(
+                onClick = { text += "+" },
+                text = "BoxButton 2",
+                leftIcon = R.drawable.ic_ground_filled,
+                customBoxColor = YdsTheme.colors.logoLightBlue,
+                buttonType = BoxButtonType.CustomColorFilled,
+            )
+            BoxButton(
+                onClick = { text += "+" },
+                text = "BoxButton 2",
+                leftIcon = R.drawable.ic_ground_filled,
+                buttonType = BoxButtonType.Line,
+            )
+            BoxButton(
+                onClick = { text += "+" },
+                text = "BoxButton 2",
+                leftIcon = R.drawable.ic_ground_filled,
+                buttonType = BoxButtonType.CustomColorLine,
+                customBoxColor = YdsTheme.colors.logoLightBlue,
             )
         }
     }
